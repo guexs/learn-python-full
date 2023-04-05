22937e8272ca2a216ca25105fb3b73504385b6c7
Santiago Guex <sgux715@gmail.com>
2023-04-04 21:47:48 -0300

    Day 1 Complete!

---
+++ Basics/00_helloworld.py
@@ -0,0 +1,101 @@
+# Este es mi primer comentario en Python
+#Estoy muy feliz de estar programando.
+#Saludos.
+
+
+print("Hello World in Python")
+print('Tambien se puede escribir con comillas simples')
+"""
+Esto es un comentario de multiples lineas
+puedo seguir escribiendo por mas que la 
+linea se corte y asi concecutivamente
+"""
+
+#Tipos de Datos:
+
+#Numeros
+#Interger - Naturales: -2, -1, 0, 1, 2
+#Float - Decimales: -2.5, -2.25, -1.0, 0.0, 1.1, 2.2
+#Complex: 1 + j o 2 + 4j
+
+#Strings - Cadenas de texto
+"""
+"Santiago"
+'Argentina'
+"Python!"
+"""
+
+#Booleans
+#True - Verdadero (not true)
+#False - Falso (not false)
+
+#List - Listas
+#Coleccion ordenada de tipos de datos
+#Pueden ser distintos.
+#[0, 1, 2, 3, 4, 5, 6]
+#['Banana', 'Orange', 'Mango', 'Apple']
+#[1, True, 'Hola', [1, 2, 3]]
+
+#Dictionary
+#Son como objetos de js - key / value
+"""
+{
+	'first_name': 'Santiago',
+	'last-name': 'Guex',
+	'age': 15,
+	'country': 'Argentina',
+	'is_married': False,
+	'skills': ['JS', 'Python', 'React']
+}
+"""
+
+#Tuplas
+#Coleccion ordenada de tipos de datos.
+#Pueden ser distintos.
+#Las Tuplas a diferencia de las List, son inmutables.
+#(0, 1, 2, 3, 4, 5, 6)
+#('Banana', 'Orange', 'Mango', 'Apple')
+#(2, False, 'Hola', [1, 2, 3]) 
+
+#Set
+#Coleccion de tipos de datos como list y tuple
+#{2.1, 3,14, 9.81}
+#{3.14, 12.06}
+
+#Type() -> Function
+print(3 + 4)
+print(4 - 3)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+print('Santiago')
+print('Nicolas, Maria, Candela')
+print('Argentina')
+print('I am enjoying 30 days of python')
+
+
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 + 4j))
+print(type(['Santiago', 'Python', 'Argentina']))
+print(type("Santiago"))
+print(type("Nicolas, Maria, Candela"))
+print(type("Argentina"))
+
+print(10)
+print(12.06)
+print(12j - 4)
+print("Hola desde Python")
+print(True)
+print(False)
+print([1, 2, 3, 4, 5, 'Hola', [True, False]])
+print((12.4, True, [1, 2, 3, 4, 5]))
+print({12.4, 4.5, 5,6})
+print({'age': 15,
+	'country': 'Argentina',
+	'is_married': False,
+	'skills': ['JS', 'Python', 'React']})
